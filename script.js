1dbc8cc3d1270a44f0a6fcd519005f314e70ef0a
elena_e <elenaka123@gmail.com>
2024-03-21 11:48:47 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Write your JavaScript below this line
+var firstName = "Elena";
+var favoriteSnack = "Chocolate chips";
+ 
+console.log("Hi, my name is " + firstName + " and I wish I had " + favoriteSnack);
+
+
+var numberOfHorses = 5;
+
+console.log(numberOfHorses);
+
+var favoriteFlower = "Sunflower";
+
+console.log("My favorite flower is a " + favoriteFlower);
+
+var cityBornIn = "Bogota";
+var birthday = "January 6";
+
+console.log("I was born on " + birthday + " in " + cityBornIn);
